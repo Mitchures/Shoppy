4756b5ec688c4af443f28f77004309efd0976992
Mitchell Hollander <MitchellSHollander@gmail.com>
2021-02-14 15:35:02 -0600

    fixed footer logo missing issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../styles/Footer.module.scss';
-import Image from 'next/image';
 import '../utils/fontawesome';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -16,7 +15,7 @@ function Footer() {
         </span>
       </div>
       <div className={styles.right}>
-        <Image src="/images/shoppy-logo-grey.svg" alt="Shoppy" width={40} height={40} />
+        <img src="/images/shoppy-logo-grey.svg" alt="Shoppy" width={40} height={40} />
         <h2>Shoppy</h2>
       </div>
     </footer>
