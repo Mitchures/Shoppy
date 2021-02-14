6ee5ceb7edbc89d64712d015573356cb6144f60e
Mitchell Hollander <MitchellSHollander@gmail.com>
2021-02-14 15:30:12 -0600

    fixed splashscreen logo missing issue

--- components/WithSplashScreen.tsx
+++ components/WithSplashScreen.tsx
@@ -1,13 +1,12 @@
 import React, { Component, ReactNode } from 'react';
 import styles from '../styles/WithSplashScreen.module.scss';
-import Image from 'next/image';
 
 function ShowSplashScreen() {
   return (
     <div id="splashScreen" className={styles.splashScreen}>
       <div className={styles.content}>
         <h1>Hello </h1>
-        <Image src="/images/shoppy-logo.svg" alt="Shoppy" width={64} height={64} />
+        <img src="/images/shoppy-logo.svg" alt="Shoppy" width={64} height={64} />
         <h1>Shoppy</h1>
       </div>
     </div>
